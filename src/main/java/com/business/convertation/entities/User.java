fe21cc43f00d210cfcd07f84d75fa8aa92a43329
LansVegans <lansxkeku@mail.ru>
2019-02-03 18:25:32 +0300

    User should called transaction.....

--- src/main/java/com/business/convertation/entities/User.java
+++ src/main/java/com/business/convertation/entities/User.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
-
+//The Entity of the meaning should be called a transaction, but it's too late ..)
 @Entity
 @Table(name = "user")
 @DynamicInsert
